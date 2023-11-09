e07bc0bb17db9f2eef64d5982eaa8472eb1f087c
melisaadakli <104603594+melisaadakli@users.noreply.github.com>
2023-11-09 23:05:21 +0300

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,32 @@
+import requests
+
+def weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'  # You can also use 'imperial' for Fahrenheit
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            print(f"\nWeather Information - {city}")
+            print("------------------------")
+            print(f"Temperature: {data['main']['temp']}°C")
+            print(f"Humidity: {data['main']['humidity']}%")
+            print(f"Wind Speed: {data['wind']['speed']} m/s")
+            print(f"Description: {data['weather'][0]['description']}")
+        else:
+            print(f"Unable to fetch weather information. Error Code: {data['cod']} - {data['message']}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    api_key = "7ac46ccea62a74163eaa6a7a74d51e52"
+    city = input("Please enter the city for weather information: ")
+    
+    weather(api_key, city)
